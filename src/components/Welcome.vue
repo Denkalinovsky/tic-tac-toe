043effc12b458d3d98768390b4fe6f0edc1cb890
DenKalinovsky <denkalinovsky@gmail.com>
2021-11-26 01:09:49 +0300

    feat(Welcome): change Welcome component, add $emit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <button
+          @click="$emit('changeDisplayOption', $event.target.value)"
+          value="newGamePlayer"
+        >
+          Start new game with another player
+        </button>
+      </li>
+      <li>
+        <button
+          @click="$emit('changeDisplayOption', $event.target.value)"
+          value="newGameBot"
+        >
+          Start new game with bot
+        </button>
+      </li>
+      <li>
+        <button
+          @click="$emit('changeDisplayOption', $event.target.value)"
+          value="loadGame"
+        >
+          Load game
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+};
+</script>
