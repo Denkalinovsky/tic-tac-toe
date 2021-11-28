a66ff998b4d3a3236f01e6e167ffc37ff5bc2951
DenKalinovsky <denkalinovsky@gmail.com>
2021-11-28 03:01:58 +0300

    feat(Vuex): add getter(getGameMode), mutation(saveGameMode), refactor

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+const gameBoard = JSON.parse(localStorage.getItem("gameBoard")) || {};
+const player = localStorage.getItem("player") || "";
+const gameMode = localStorage.getItem("gameMode") || "";
+
 Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
-    gameBoard: {},
-    player: "",
+    gameBoard,
+    player,
+    gameMode,
   },
   mutations: {
     /**
-     * saving the playing field and who will walk at this point
+     * Save the playing field, who will walk
      * @param: {object} state
      * @param: {object} gameBoard - contains the playing field
      * @param: {string} player - contains the who walks
@@ -17,20 +22,37 @@ export default new Vuex.Store({
     saveGame(state, { gameBoard, player }) {
       state.gameBoard = gameBoard;
       state.player = player;
+      localStorage.setItem("gameBoard", JSON.stringify(gameBoard));
+      localStorage.setItem("player", player);
+    },
+    /**
+     * Save game mode
+     * @param: {object} state
+     * @param: {string} gameMode
+     */
+    saveGameMode(state, gameMode) {
+      state.gameMode = gameMode;
+      localStorage.setItem("gameMode", gameMode);
     },
   },
   getters: {
     /**
-     * get game board
+     * Get game board
      * @param: {object} state
      * @returns: {object} gameBoard
      */
     getGameBoard: (state) => state.gameBoard,
     /**
-     * get who walks
+     * Get who walks
      * @param: {object} state
      * @returns: {string} player
      */
-    getWhoWalks: (state) => state.player,
+    getPlayer: (state) => state.player,
+    /**
+     * Get game mode
+     * @param: {object} state
+     * @returns: {string} gameMode
+     */
+    getGameMode: (state) => state.gameMode,
   },
 });
